5877d4f634e6c397a12f904e9f874d3a71ad4a9a
teknotel17 <terryarif@googlemail.com>
2025-03-25 21:41:24 +0000

    Fix: corrected casing for banterfacts import

--- src/pages/TrophyCabinet.jsx
+++ src/pages/TrophyCabinet.jsx
@@ -1,7 +1,7 @@
 // src/TrophyCabinet.jsx
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import { getRandomFact } from "../utils/banterFacts";
+import { getRandomFact } from "../utils/banterfacts";
 
 const TrophyCabinet = () => {
   const [fact, setFact] = useState("");
